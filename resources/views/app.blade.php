4ac22182544064c1986c5741a0ab731f8ef7cd6e
martinvintherp <martinvintherp@gmail.com>
2021-05-31 20:54:36 +0200

    Add 'root' files for rendering our react component

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="content">
+            <div id="app"></div>
+        </div>
+
+    </div>
+</div>
+@endsection
